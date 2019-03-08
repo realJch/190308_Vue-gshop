bc5d6b6347efc70ebc6534229ac82092ea3b5334
jichanghu <1877133476@qq.com>
2019-03-08 09:39:27 +0800

    init vue-test

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+      <router-view></router-view>
+      <FootGuide v-show="$route.meta.showFooter"/>
+    </div>
+</template>
+
+<script>
+    import FootGuide from './components/FooterGuide/FooterGuide.vue'
+    export default {
+      //映射成标签
+      components: {
+        FootGuide
+      }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    width 100%
+    height 100%
+    background #f5f5f5
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import vueRouter from 'vue-router'
+import MSite from '../pages/MSite/MSite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Login from "../pages/Login/Login";
+//声明使用插件
+Vue.use(vueRouter)
+
+export default new vueRouter({
+  routes: [
+    {
+      path: '/MSite',
+      component: MSite,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/order',
+      component:Order,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile
+      ,meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/',
+      redirect: '/MSite'
+    },
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
+})
